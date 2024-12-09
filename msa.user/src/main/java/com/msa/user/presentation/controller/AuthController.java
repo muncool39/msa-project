753d323a91651bbc37426503a9f78bd254d45d0b
siwon <muncool39@gmail.com>
2024-12-09 16:29:06 +0900

    [Feat] 회원 가입 API

---
+++ msa.user/src/main/java/com/msa/user/presentation/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.msa.user.presentation.controller;
+
+import com.msa.user.application.service.AuthService;
+import com.msa.user.presentation.request.SignUpRequest;
+import com.msa.user.presentation.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ApiResponse<Void> signUp(
+            @Valid @RequestBody SignUpRequest signUpRequest
+    ) {
+        authService.createUser(signUpRequest);
+        return ApiResponse.success();
+    }
+
+}
