fe4c13e6832dcfbeee27573363b0e60d7ed1d299
jaehee-An <jhh9615@gmail.com>
2024-12-10 00:28:54 +0900

    [feat] order entity 설계

---
+++ msa.order/src/main/java/com/msa/order/domain/entity/BaseEntity.java
@@ -0,0 +1,47 @@
+package com.msa.order.domain.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@CreatedBy
+	@Column(updatable = false)
+	private Long createdBy;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
+	@LastModifiedBy
+	private Long updatedBy;
+
+	private LocalDateTime deletedAt;
+
+	private Long deletedBy;
+
+	private LocalDateTime canceledAt;
+
+	private Long canceledBy;
+
+	@ColumnDefault("false")
+	private Boolean isDeleted;
+
+}
---
+++ msa.order/src/main/java/com/msa/order/domain/entity/Order.java
@@ -0,0 +1,68 @@
+package com.msa.order.domain.entity;
+
+import java.util.UUID;
+
+import com.msa.order.domain.entity.enums.OrderStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "p_order")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PRIVATE)
+@Getter
+public class Order extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(nullable = false)
+	private UUID supplierCompanyId;
+
+	@Column(nullable = false)
+	private UUID receiverCompanyId;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+	@Column(nullable = false)
+	private UUID itemId;
+
+	@Column(nullable = false)
+	private int quantity;
+
+	private String description;
+
+	@Column(nullable = false)
+	private UUID deliveryId;
+
+
+	public static Order create(UUID supplierCompanyId, UUID receiverCompanyId, UUID itemId, int quantity,
+		String description, UUID deliveryId) {
+
+		return Order.builder()
+			.supplierCompanyId(supplierCompanyId)
+			.receiverCompanyId(receiverCompanyId)
+			.status(OrderStatus.ORDERED)
+			.itemId(itemId)
+			.quantity(quantity)
+			.description(description)
+			.deliveryId(deliveryId)
+			.build();
+	}
+}
