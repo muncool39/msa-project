735ac71ade8dd273d4a9a4efad883bb9c3166481
siwon <muncool39@gmail.com>
2024-12-09 11:40:24 +0900

    [Init] 프로젝트 인프라

---
+++ msa.notification/src/main/java/com/msa/notification/NotificationApplication.java
@@ -0,0 +1,15 @@
+package com.msa.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+
+}
