707b4cb6e5ea67299718c960d7a237a0ab0a8f0f
siwon <muncool39@gmail.com>
2024-12-10 01:02:56 +0900

    [Feat] Hub 엔티티 구성

---
+++ msa.hub/src/main/java/com/msa/hub/domain/model/Hub.java
@@ -0,0 +1,65 @@
+package com.msa.hub.domain.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "p_hub",
+        uniqueConstraints = {
+        @UniqueConstraint(name = "UK_HUB_NAME", columnNames = "name")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hub extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    @Column(name="id")
+    private String id;
+
+    @Column(name="name", nullable=false)
+    private String name;
+
+    @Column(name="city", nullable=false)
+    private String city;
+
+    @Column(name="district", nullable=false)
+    private String district;
+
+    @Column(name="street_name", nullable=false)
+    private String streetName;
+
+    @Column(name="street_number", nullable=false)
+    private String streetNumber;
+
+    @Column(name="address_detail", nullable=false)
+    private String addressDetail;
+
+    @Column(name="latitude", nullable=false)
+    private Double latitude;
+
+    @Column(name="longitude", nullable=false)
+    private Double longitude;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Hub(String name, String city, String district, String streetName, String streetNumber, String addressDetail,
+               Double latitude, Double longitude) {
+        this.name = name;
+        this.city = city;
+        this.district = district;
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+        this.addressDetail = addressDetail;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
