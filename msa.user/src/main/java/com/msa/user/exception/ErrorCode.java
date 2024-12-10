91398bf9b0d794226dd04ebe4d535a5abbeee3a4
siwon <muncool39@gmail.com>
2024-12-10 11:57:51 +0900

    [Feat] 비밀번호가 일치하지 않는 경우의 오류 코드 추가

--- msa.user/src/main/java/com/msa/user/exception/ErrorCode.java
+++ msa.user/src/main/java/com/msa/user/exception/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
     DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 사용자입니다.");
     private final HttpStatus httpStatus;
     private final String message;
