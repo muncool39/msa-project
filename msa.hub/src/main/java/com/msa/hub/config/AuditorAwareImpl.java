e66b4dd0056e288cd22217de50bb765cce6c355d
시원 <105481797+muncool39@users.noreply.github.com>
2024-12-10 12:10:27 +0900

    [Feat] 허브 서비스 도메인 모델 구성 (#18)
    * [Build] jpa 의존성 추가
    
    * [Feat] 로그 기록을 위한 BaseEntity 설정
    
    * [Feat] Hub 엔티티 구성
    
    * [Feat] 허브 루트 엔티티 구성
    
    * [Feat] 유효성 검사 API 틀
    
    * [Fix] 허브와 허브 경로 일대다 매핑 추가

---
+++ msa.hub/src/main/java/com/msa/hub/config/AuditorAwareImpl.java
@@ -0,0 +1,11 @@
+package com.msa.hub.config;
+
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.empty();
+    }
+}
---
+++ msa.hub/src/main/java/com/msa/hub/domain/model/BaseEntity.java
@@ -0,0 +1,45 @@
+package com.msa.hub.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Column(name="is_deleted")
+    @ColumnDefault("false")
+    private Boolean isDeleted;
+
+    @CreatedDate
+    @Column(name="created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name="updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name="deleted_at")
+    private LocalDateTime deletedAt;
+
+    @CreatedBy
+    @Column(name="created_by")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name="updated_by")
+    private String updatedBy;
+
+    @Column(name="deleted_by")
+    private String deletedBy;
+
+}
