f6e33584baedc9f2b296e14196a464e160f04346
jaehee-An <jhh9615@gmail.com>
2024-12-12 01:28:05 +0900

    [feat] security 설정

---
+++ msa.order/src/main/java/com/msa/order/config/UserDetailImpl.java
@@ -0,0 +1,36 @@
+package com.msa.order.config;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.msa.order.domain.entity.enums.UserRole;
+
+public record UserDetailImpl(
+	String userId,
+	String role
+
+) implements UserDetails {
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority(role));
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return userId; // user pk
+	}
+
+	public UserRole getUserRole() {
+		return UserRole.fromString(role);
+	}
+}
