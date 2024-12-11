f35fe2936321d79c27b8110aa1ab02e8346d4782
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-12-12 01:21:29 +0900

    feat: SlackNotification 엔티티 추가 (#35)

---
+++ msa.notification/src/main/java/com/msa/notification/domain/SlackNotification.java
@@ -0,0 +1,45 @@
+package com.msa.notification.domain;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+@Table(name="slack_notification")
+public class SlackNotification {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String receiptId;
+
+    @Column(nullable = false)
+    private String message;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime sentAt;
+}
