ff48c25ec44194aa2bdde4e5992f0c8dcb2b49d0
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-12-12 01:21:05 +0900

    feat: MSA Notification 모듈에 JPA 및 AiRequest 엔터티 추가(#35)

---
+++ msa.notification/src/main/java/com/msa/notification/domain/AiRequest.java
@@ -0,0 +1,36 @@
+package com.msa.notification.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="ai_request")
+public class AiRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, columnDefinition = "JSON")
+    private String requestData; // 요청 데이터(JSON)
+
+    @Column(nullable = false)
+    private String answer; // 응답 데이터
+
+    @Column(nullable = false)
+    private Boolean isDeleted = false;
+
+}
