07aaf2ded2ff7bd2aec0d26ce2c84d89de81ae1a
jaehee-An <jhh9615@gmail.com>
2024-12-13 01:54:30 +0900

    [feat] 시큐리티 설정 및 예외핸들러 추가

---
+++ msa.delivery/src/main/java/com/msa/delivery/config/AuditorAwareImpl.java
@@ -0,0 +1,26 @@
+package com.msa.delivery.config;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (null == authentication || !authentication.isAuthenticated()) {
+			return Optional.empty();
+		}
+
+		if (authentication.getName().equals("anonymousUser")) {
+			return Optional.of("anonymousUser");
+		}
+
+		return Optional.of(authentication.getName());
+	}
+}
+
+
