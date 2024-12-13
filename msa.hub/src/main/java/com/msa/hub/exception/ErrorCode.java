636369310f99b7b5b9bff84c086a7dca89146d06
siwon <muncool39@gmail.com>
2024-12-13 16:19:32 +0900

    [Feat] 지원하지 않는 정렬 방식인 경우의 오류 코드 추가

--- msa.hub/src/main/java/com/msa/hub/exception/ErrorCode.java
+++ msa.hub/src/main/java/com/msa/hub/exception/ErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    UNSUPPORTED_SORT_TYPE(HttpStatus.BAD_REQUEST, "지원하지 않는 정렬 방식입니다."),
     INVALID_ROLE(HttpStatus.UNAUTHORIZED, "허브 관리자 권한이 존재하지 않습니다."),
     DUPLICATE_HUB_NAME(HttpStatus.BAD_REQUEST, "이미 존재하는 허브입니다."),
     HUB_NOT_FOUND(HttpStatus.NOT_FOUND, "일치하는 허브 정보를 찾을 수 없습니다.");
