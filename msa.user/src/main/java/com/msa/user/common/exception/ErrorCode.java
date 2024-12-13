26512073b7c416fd665e6da74fe1513355026b4b
siwon <muncool39@gmail.com>
2024-12-14 01:44:18 +0900

    [Feat] 유저 소속 허브 설정 API

--- msa.user/src/main/java/com/msa/user/common/exception/ErrorCode.java
+++ msa.user/src/main/java/com/msa/user/common/exception/ErrorCode.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+
+    INVALID_HUB(HttpStatus.BAD_REQUEST, "유효하지 않은 허브입니다."),
     INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "일치하는 사용자 정보가 존재하지 않습니다."),
     DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "이미 존재하는 사용자입니다.");
--- msa.user/src/main/java/com/msa/user/presentation/controller/UserController.java
+++ msa.user/src/main/java/com/msa/user/presentation/controller/UserController.java
@@ -2,10 +2,15 @@ package com.msa.user.presentation.controller;
 
 import com.msa.user.application.dto.UserDetailResponse;
 import com.msa.user.application.service.UserService;
+import com.msa.user.presentation.request.SetBelongHubRequest;
 import com.msa.user.presentation.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +26,15 @@ public class UserController {
             @PathVariable Long userId
     ) {
         return ApiResponse.success(userService.getUser(userId));
+    }
+
+    @PostMapping("/belong-hub")
+    @PreAuthorize("hasAuthority('MASTER')")
+    public ApiResponse<Void> postUserBelongHub(
+            @Valid @RequestBody SetBelongHubRequest request
+    ) {
+        userService.setBelongHub(request.userId(), request.hubId());
+        return ApiResponse.success();
     }
 
 }
