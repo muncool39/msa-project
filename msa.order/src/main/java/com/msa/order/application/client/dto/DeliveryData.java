4cafe6575eb0e2f514754b27a495e32e021da951
jaehee-An <jhh9615@gmail.com>
2024-12-14 15:21:43 +0900

    [feat] deliveryClient 설정 수정

--- msa.order/src/main/java/com/msa/order/application/client/dto/DeliveryData.java
+++ msa.order/src/main/java/com/msa/order/application/client/dto/DeliveryData.java
@@ -3,7 +3,7 @@ package com.msa.order.application.client.dto;
 import java.util.UUID;
 
 public record DeliveryData(
-    Long deliveryId,
+	UUID deliveryId,
     UUID orderId,
     String status,
 	UUID departureHubId,
