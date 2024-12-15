a92785bdd8b857e6a587246430df5fa9455a8385
Hujin0322 <hongyuj0322@naver.com>
2024-12-16 02:36:23 +0900

    [Feat] 상품 전체 조회 기능 구현 ( #52 )

--- msa.company/src/main/java/com/msa/company/application/service/ProductService.java
+++ msa.company/src/main/java/com/msa/company/application/service/ProductService.java
@@ -1,14 +1,10 @@
 package com.msa.company.application.service;
 
-import static com.msa.company.exception.ErrorCode.COMPANY_NOT_FOUND;
-
-import com.msa.company.domain.entity.Company;
-import com.msa.company.domain.entity.Product;
-import com.msa.company.domain.repository.CompanyRepository;
 import com.msa.company.domain.repository.ProductRepository;
-import com.msa.company.exception.CompanyException;
 import com.msa.company.infrastructure.HubClient;
-import com.msa.company.presentation.request.ProductRequest;
+import com.msa.company.presentation.response.ProductListResponse;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,29 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductService {
 
     private final ProductRepository productRepository;
-    private final CompanyRepository companyRepository;
     private final HubClient hubClient;
 
-
-    @Transactional
-    public void createProduct(ProductRequest productRequest, Long userId, String role) {
-
-        // 업체 존재 여부 확인
-        Company company = companyRepository.findById(productRequest.companyId())
-                .orElseThrow(() -> new CompanyException(COMPANY_NOT_FOUND));
-
-        /* TODO 1. HUB_MANAGER: 본인 허브만 생성 가능
-        if ("HUB_MANAGER".equals(role)) {
-            UUID hubId = company.getHubId();
-        } else if ("COMPANY_MANAGER".equals(role)) {
-           TODO 2. COMPANY_MANAGER: 본인 업체만 생성 가능
-        }*/
-
-        Product product = Product.create(
-                productRequest,
-                company,
-                userId
-        );
-        productRepository.save(product);
+    // 상품 전체 조회
+    @Transactional(readOnly = true)
+    public List<ProductListResponse> getListProducts() {
+        return productRepository.findAll().stream()
+                .map(ProductListResponse::from)
+                .collect(Collectors.toList());
     }
 }
--- msa.company/src/main/java/com/msa/company/presentation/controller/ProductController.java
+++ msa.company/src/main/java/com/msa/company/presentation/controller/ProductController.java
@@ -1,34 +1,24 @@
 package com.msa.company.presentation.controller;
 
 import com.msa.company.application.service.ProductService;
-import com.msa.company.presentation.request.ProductRequest;
 import com.msa.company.presentation.response.ApiResponse;
-import jakarta.validation.Valid;
+import com.msa.company.presentation.response.ProductListResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/products")
 public class ProductController {
 
     public final ProductService productService;
 
-    // 상품 생성
-    @PostMapping
-    @PreAuthorize("hasAnyAuthority('MASTER', 'HUB_MANAGER','COMPANY_MANAGER')")
-    public ApiResponse<Void> createProduct(
-            @RequestBody @Valid ProductRequest productRequest,
-            @AuthenticationPrincipal UserDetails userDetail) {
-        Long userId = Long.valueOf(userDetail.getUsername());
-        String role = userDetail.getAuthorities().iterator().next().getAuthority();
-        productService.createProduct(productRequest, userId, role);
-        return ApiResponse.success();
+    // 상품 전체 조회
+    @GetMapping("/products")
+    public ApiResponse<List<ProductListResponse>> getListProducts() {
+        List<ProductListResponse> products = productService.getListProducts();
+        return ApiResponse.success(products);
     }
+
 }
