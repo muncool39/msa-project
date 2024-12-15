30546d8a5c44ed282234b6434f5845788f9cbdc3
Hujin0322 <hongyuj0322@naver.com>
2024-12-16 02:22:55 +0900

    [Rename] 파일 이름 변경 및 필요 없는 코드 삭제  ( #52 )

--- msa.company/src/main/java/com/msa/company/presentation/request/CreateProductRequest.java
+++ msa.company/src/main/java/com/msa/company/presentation/request/CreateProductRequest.java
@@ -3,19 +3,15 @@ package com.msa.company.presentation.request;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import java.util.UUID;
 import lombok.Builder;
 
 @Builder
-public record ProductRequest(
+public record CreateProductRequest(
 
-        @NotNull(message = "업체 ID는 필수입니다.")
-        UUID companyId,
-
-        @NotBlank(message = "상품명은 필수입니다.")
+        @NotBlank(message = "상품명을 필수로 입력해주세요.")
         String name,
 
-        @NotNull(message = "재고 수량은 필수입니다.")
+        @NotNull(message = "재고 수량을 필수로 입력해주세요.")
         @Min(value = 0, message = "재고 수량은 0 이상이어야 합니다.")
         Long stock
 ) {
