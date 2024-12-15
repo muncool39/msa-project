43ac1afe1ed5b084818938f30d4c447454c56618
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-12-15 22:27:46 +0900

    feat: Gemini 클라이언트 통신 기능 추가

---
+++ msa.notification/src/main/java/com/msa/notification/infrastructure/dto/GeminiClientRequestDto.java
@@ -0,0 +1,24 @@
+package com.msa.notification.infrastructure.dto;
+
+import java.util.Collections;
+import java.util.List;
+import lombok.Getter;
+
+public record GeminiClientRequestDto(
+        List<Content> contents
+) {
+    @Getter
+        public record Content(List<Part> parts) {
+    }
+
+    @Getter
+        public record Part(String text) {
+    }
+
+    public static GeminiClientRequestDto create(String prompt, String maxLengthPromptMessage) {
+        Part part = new Part(prompt + maxLengthPromptMessage);
+        Content content = new Content(Collections.singletonList(part));
+
+        return new GeminiClientRequestDto(Collections.singletonList(content));
+    }
+}
