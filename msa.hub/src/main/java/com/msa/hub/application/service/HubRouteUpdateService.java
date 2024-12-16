8226616f27dc3a6777b0569fee6d25b2418bd81b
시원 <105481797+muncool39@users.noreply.github.com>
2024-12-17 00:24:54 +0900

    [Feat] 허브 루트 상세 수정 API (#91)
    * [Feat] 일치하는 경유지 정보가 없는 경우의 오류 코드 추가
    
    * [Feat] 경로 업데이트 요청 객체 추가
    
    * [Feat] 각 엔티티에 업데이트 메서드 추가
    
    * [Feat] 경유지 레포지토리 추가
    
    * [Feat] 허브 경로 수정 API

---
+++ msa.hub/src/main/java/com/msa/hub/application/service/HubRouteUpdateService.java
@@ -0,0 +1,37 @@
+package com.msa.hub.application.service;
+
+import com.msa.hub.domain.model.HubRoute;
+import com.msa.hub.domain.model.Waypoint;
+import com.msa.hub.domain.repository.HubRouteRepository;
+import com.msa.hub.domain.repository.WaypointRepository;
+import com.msa.hub.exception.ErrorCode;
+import com.msa.hub.exception.HubException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class HubRouteUpdateService {
+    private final HubRouteRepository hubRouteRepository;
+    private final WaypointRepository waypointRepository;
+
+    public void updateHubRouteDetail(String routeId, Double totalDistance, Long totalDuration) {
+        HubRoute hubRoute = hubRouteRepository.findById(routeId)
+                .orElseThrow(()-> new HubException(ErrorCode.HUB_ROUTE_NOT_FOUND));
+        hubRoute.updateTotalDetail(totalDistance, totalDuration);
+    }
+
+    public void updateWaypointDetail(
+            String waypointId, Double distanceFromPrevious, Integer durationFromPrevious
+    ) {
+        Waypoint waypoint = waypointRepository.findById(waypointId)
+                .orElseThrow(()-> new HubException(ErrorCode.WAYPOINT_NOT_FOUND));
+        waypoint.updateDetail(distanceFromPrevious, durationFromPrevious);
+        /* TODO: 허브 루트 총 거리 수정
+        HubRoute hubRoute = hubRouteRepository.findById(waypoint.getLinkedRoute().getId())
+               .orElseThrow(()-> new HubException(ErrorCode.HUB_ROUTE_NOT_FOUND));
+         */
+    }
+}
