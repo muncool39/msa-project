37323e86baae9ab22d22eea51d1f1ac7a74cd769
Hujin0322 <hongyuj0322@naver.com>
2024-12-16 10:56:35 +0900

    [Refactor] @RequestMapping("/products") 추가 및 재고 관련 로직을 patchmapping으로 변경 ( #52 )

--- msa.company/src/main/java/com/msa/company/presentation/controller/ProductController.java
+++ msa.company/src/main/java/com/msa/company/presentation/controller/ProductController.java
@@ -15,32 +15,33 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
 
     public final ProductService productService;
 
     // 상품 전체 조회
-    @GetMapping("/products")
+    @GetMapping
     public ApiResponse<List<ProductListResponse>> getListProducts() {
         List<ProductListResponse> products = productService.getListProducts();
         return ApiResponse.success(products);
     }
 
     // 상품 단건 조회
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public ApiResponse<ProductDetailResponse> getDetailProduct(@PathVariable("id") UUID id) {
         ProductDetailResponse product = productService.getDetailProduct(id);
         return ApiResponse.success(product);
     }
 
     // 상품 수정
-    @PatchMapping("/products/{id}")
+    @PatchMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('MASTER', 'HUB_MANAGER','COMPANY_MANAGER')")
     public ApiResponse<Void> updateProduct(@PathVariable("id") UUID id,
                                            @RequestBody UpdateProductRequest request,
@@ -51,7 +52,7 @@ public class ProductController {
     }
 
     // 상품 삭제
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('MASTER', 'HUB_MANAGER')")
     public ApiResponse<Void> deleteProduct(@PathVariable("id") UUID id,
                                            @AuthenticationPrincipal Long userId,
@@ -61,15 +62,17 @@ public class ProductController {
     }
 
     // 상품 재고 감소
-    @PostMapping("/products/{id}/reduce-stock")
-    public ApiResponse<Void> decreaseStock(@PathVariable("id") UUID id, @RequestBody StockRequest stockRequest) {
+    @PatchMapping("/{id}/reduce-stock")
+    public ApiResponse<Void> decreaseStock(@PathVariable("id") UUID id,
+                                           @RequestBody StockRequest stockRequest) {
         productService.decreaseStock(id, stockRequest.getStock());
         return ApiResponse.success();
     }
 
     // 상품 재고 복원
-    @PostMapping("/products/{id}/restore-stock")
-    public ApiResponse<Void> restoreStock(@PathVariable("id") UUID id, @RequestBody StockRequest stockRequest) {
+    @PatchMapping("/{id}/restore-stock")
+    public ApiResponse<Void> restoreStock(@PathVariable("id") UUID id,
+                                          @RequestBody StockRequest stockRequest) {
         productService.restoreStock(id, stockRequest.getStock());
         return ApiResponse.success();
     }
