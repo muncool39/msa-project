e195ad165735050a2dba8d1bc931a8e4e33c51ba
siwon <muncool39@gmail.com>
2024-12-17 12:44:58 +0900

    [Feat] 허브 삭제 API

--- msa.hub/src/main/java/com/msa/hub/application/service/HubService.java
+++ msa.hub/src/main/java/com/msa/hub/application/service/HubService.java
@@ -4,6 +4,7 @@ package com.msa.hub.application.service;
 import com.msa.hub.application.dto.HubBasicResponse;
 import com.msa.hub.application.dto.HubDetailResponse;
 import com.msa.hub.domain.model.Hub;
+import com.msa.hub.domain.model.HubRoute;
 import com.msa.hub.domain.repository.HubRepository;
 import com.msa.hub.common.exception.ErrorCode;
 import com.msa.hub.common.exception.HubException;
@@ -12,6 +13,7 @@ import com.msa.hub.presentation.request.HubUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -23,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class HubService {
 
     private final HubRepository hubRepository;
+    private final UserService userService;
 
     @Transactional
     @CacheEvict(cacheNames = "hub_list_cache", allEntries = true)
@@ -75,6 +78,19 @@ public class HubService {
     public void updateHubManager(final String hubId, final Long userId) {
         Hub hub = getHubOrException(hubId);
         hub.setManager(userId);
+    }
+
+    @Transactional
+    @Caching(evict = {
+            @CacheEvict(cacheNames = "hub_list_cache", allEntries = true),
+            @CacheEvict(cacheNames = "hub_routes_cache", allEntries = true)
+    })
+    public void deleteHub(final String hubId, final Long userId) {
+        Hub hub = getHubOrException(hubId);
+        if(hub.getManagerId() != null) {
+            userService.detachBelongHub(hub.getManagerId());
+        }
+        hub.softDeleteHub(userId);
     }
 
     private Hub getHubOrException(final String hubId) {
