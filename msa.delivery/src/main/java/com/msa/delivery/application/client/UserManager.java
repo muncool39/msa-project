2e5ee91021436377816c07aa25c6d2f5df1b5763
jaehee-An <jhh9615@gmail.com>
2024-12-17 14:33:42 +0900

    [feat] 배송 서비스 fallbackFactory 클래스 도입

--- msa.delivery/src/main/java/com/msa/delivery/application/client/UserManager.java
+++ msa.delivery/src/main/java/com/msa/delivery/application/client/UserManager.java
@@ -1,7 +1,8 @@
 package com.msa.delivery.application.client;
 
-import com.msa.delivery.application.client.dto.response.DeliveryWorkersData;
 import com.msa.delivery.application.client.dto.request.GetDeliveryWorkersRequest;
+import com.msa.delivery.application.client.dto.response.DeliveryWorkersData;
+import com.msa.delivery.application.client.dto.response.ShipperData;
 import com.msa.delivery.application.client.dto.response.UserData;
 import com.msa.delivery.presentation.response.ApiResponse;
 
@@ -9,5 +10,7 @@ public interface UserManager {
 
 	ApiResponse<UserData> getUserInfo(Long userId);
 
-	DeliveryWorkersData assignDeliveryWorkers(GetDeliveryWorkersRequest request);
+	ApiResponse<DeliveryWorkersData> assignDeliveryWorkers(GetDeliveryWorkersRequest request);
+
+	ApiResponse<ShipperData> getShipperInfo(Long deliverId);
 }
