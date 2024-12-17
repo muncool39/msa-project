21d30f060a6414c7248831bded79f49803f22d0b
jaehee-An <jhh9615@gmail.com>
2024-12-17 14:33:42 +0900

    [feat] 배송 서비스 fallbackFactory 클래스 도입

--- msa.delivery/src/main/java/com/msa/delivery/infrastructure/client/fallback/UserClientFallback.java
+++ msa.delivery/src/main/java/com/msa/delivery/infrastructure/client/fallback/UserClientFallback.java
@@ -2,25 +2,27 @@ package com.msa.delivery.infrastructure.client.fallback;
 
 import org.springframework.stereotype.Component;
 
-import com.msa.delivery.application.client.dto.response.DeliveryWorkersData;
 import com.msa.delivery.application.client.dto.request.GetDeliveryWorkersRequest;
+import com.msa.delivery.application.client.dto.response.DeliveryWorkersData;
+import com.msa.delivery.application.client.dto.response.ShipperData;
 import com.msa.delivery.application.client.dto.response.UserData;
 import com.msa.delivery.infrastructure.client.impl.UserClient;
 import com.msa.delivery.presentation.response.ApiResponse;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j(topic = "User-Client")
 @Component("userClientFallback")
 public class UserClientFallback implements UserClient {
 	@Override
 	public ApiResponse<UserData> getUserInfo(Long userId) {
-		log.error("[FeignClient] 유저 정보 호출 에러 발생");
-		return null;
+		return ApiResponse.fail(null);
 	}
 
 	@Override
-	public DeliveryWorkersData assignDeliveryWorkers(GetDeliveryWorkersRequest request) {
-		return null;
+	public ApiResponse<DeliveryWorkersData> assignDeliveryWorkers(GetDeliveryWorkersRequest request) {
+		return ApiResponse.fail(null);
+	}
+
+	@Override
+	public ApiResponse<ShipperData> getShipperInfo(Long deliverId) {
+		return ApiResponse.fail(null);
 	}
 }
